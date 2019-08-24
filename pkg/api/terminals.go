ed673fe9aee5e2c70a0018ed19688074b4b4f8ec
Spyridon Pagkalos <spyridon@ender.gr>
2019-08-24 23:10:41 +0300

    Refactored terminals API

--- pkg/api/terminals.go
+++ pkg/api/terminals.go
@@ -1,60 +1,38 @@
 package api
 
 import (
+	"fmt"
+	"net/http"
+	"strconv"
+
 	"github.com/enderian/directrd/pkg/types"
 	"github.com/labstack/echo"
-	"net/http"
 )
 
 func terminalsGroup(g *echo.Group) {
-	g.GET("/all", terminalsAll)
-	g.GET("/room/:id", getRoomStatus)
-	g.GET("/:terminal", getSingleTerminal)
-	g.POST("/:terminal/execute", execCommand)
+	g.GET("s", terminalsAll)
 	g.POST("/", registerTerminal)
-	g.DELETE("/:terminal", deleteTerminal)
-	g.PUT("/:terminal", updateTerminal)
-}
 
+	term := g.Group("/:name", setTerminal)
+	term.GET("", getSingleTerminal)
+	term.DELETE("", deleteTerminal)
+	term.PUT("", updateTerminal)
+	term.POST("/execute", execCommand)
+}
 func terminalsAll(c echo.Context) error {
 	var terminals []*types.Terminal
-	if err := ctx.DB().Find(&terminals).Error; err != nil {
+	query := ctx.DB()
+
+	if c.QueryParam("room") != "" {
+		room, _ := strconv.Atoi(c.QueryParam("room"))
+		query = query.Where("room_id = ?", room)
+	}
+
+	if err := query.Find(&terminals).Error; err != nil {
 		panic(err)
 	}
 
 	return c.JSON(http.StatusOK, terminals)
-}
-
-func getRoomStatus(c echo.Context) error {
-	var terminals []*types.Terminal
-	room := c.Param("id")
-
-	if err := ctx.DB().Where("room_id = ?", room).Find(&terminals).Error; err != nil {
-		panic(err)
-	}
-
-	return c.JSON(http.StatusOK, terminals)
-}
-
-func getSingleTerminal(c echo.Context) error {
-	var term []*types.Terminal
-	terminal := c.Param("terminal")
-
-	if err := ctx.DB().Where("name = ?", terminal).Find(&term).Error; err != nil {
-		panic(err)
-	}
-
-	return c.JSON(http.StatusOK, term)
-}
-
-func execCommand(c echo.Context) error {
-	cmd := types.Command{}
-	cmd.Terminal = c.Param("terminal")
-	_ = c.Bind(&cmd)
-
-	commandQueue <- cmd
-
-	return c.NoContent(http.StatusNoContent)
 }
 
 func registerTerminal(c echo.Context) error {
@@ -68,27 +46,49 @@ func registerTerminal(c echo.Context) error {
 	return c.JSON(http.StatusOK, terminal)
 }
 
-func deleteTerminal(c echo.Context) error {
-	name := c.Param("terminal")
-	terminal := &types.Terminal{Name: name}
+var setTerminal echo.MiddlewareFunc = func(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
 
-	if err := ctx.DB().Delete(&terminal).Error; err != nil {
-		return err
+		terminal := &types.Terminal{}
+		if err := ctx.DB().Where("name = ?", c.Param("name")).Find(&terminal).Error; err != nil {
+			return fmt.Errorf("terminal with name %s not found", c.Param("name"))
+		}
+
+		c.Set("terminal", terminal)
+		return next(c)
 	}
+}
+
+func getSingleTerminal(c echo.Context) error {
+	return c.JSON(http.StatusOK, c.Get("terminal").(*types.Terminal))
+}
+
+func execCommand(c echo.Context) error {
+	cmd := types.Command{}
+	cmd.Terminal = c.Get("terminal").(*types.Terminal).Name
+	_ = c.Bind(&cmd)
+
+	commandQueue <- cmd
 
 	return c.NoContent(http.StatusNoContent)
 }
 
+func deleteTerminal(c echo.Context) error {
+	if err := ctx.DB().Delete(c.Get("terminal").(*types.Terminal)).Error; err != nil {
+		return err
+	}
+	return c.NoContent(http.StatusNoContent)
+}
+
 func updateTerminal(c echo.Context) error {
-	name := c.Param("terminal")
 	terminal := &types.Terminal{}
-	ctx.DB().Where("name = ?", name).Find(terminal)
+	terminal.ID = c.Get("terminal").(*types.Terminal).ID
 
-	_ = c.Bind(&terminal)
-
+	if err := c.Bind(&terminal); err != nil {
+		return err
+	}
 	if err := ctx.DB().Save(&terminal).Error; err != nil {
 		return err
 	}
-
-	return c.NoContent(http.StatusNoContent)
+	return c.JSON(http.StatusOK, terminal)
 }
