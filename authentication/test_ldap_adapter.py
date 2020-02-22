310a634e3c30355f09c0db38dd8b9ab6adce628b
Spyridon Pagkalos <spyridon@ender.gr>
2020-02-22 19:51:22 +0200

    Added docker-compose testing, and whitespace changes

---
+++ authentication/test_ldap_adapter.py
@@ -0,0 +1,28 @@
+import pytest
+
+from app import db
+from model import User
+from authentication.ldap_adapter import LdapAdapter
+
+
+@pytest.fixture
+def ldap_adapter(app):
+    yield LdapAdapter()
+
+
+def test_bind(ldap_adapter):
+    def assertion(conn):
+        assert conn is not None
+
+    ldap_adapter._LdapAdapter__with_connection(assertion)
+
+
+def test_get_user(ldap_adapter):
+    assert ldap_adapter.get_user('admin') is not None
+    assert ldap_adapter.get_user('user') is None
+
+
+def test_search_user(ldap_adapter):
+    assert len(ldap_adapter.search_user('admin')) == 1
+    assert len(ldap_adapter.search_user('user')) == 0
+
