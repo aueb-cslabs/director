d8a76c1b4cdc0e7640e8f3b9cfadcb4167d40d0e
Spyridon Pagkalos <spyridon@ender.gr>
2020-02-22 18:05:13 +0200

    Reorganize fictures and add transaction rollback

--- api/test_public.py
+++ api/test_public.py
@@ -6,10 +6,13 @@ from model import User
 
 def test_get_user(client):
     db.session.add(User(username='p3150133', full_name='Spyridon Pagkalos'))
-    db.session.commit()
 
     rv = client.get('/api/public/user/p3150133')
-
     assert b'p3150133' in rv.data
     assert b'Spyridon Pagkalos' in rv.data
+
+
+def test_get_user_failing_test(client):
+    rv = client.get('/api/public/user/p3150133')
+    assert b'{}\n' == rv.data
 
--- conftest.py
+++ conftest.py
@@ -1,12 +1,11 @@
 import os
 import tempfile
-
 import pytest
 
-from app import create_app
+from app import create_app, db
 
-@pytest.fixture(scope="session")
-def app():
+@pytest.fixture(scope='session', autouse=True)
+def app_primitive():
     db_fd, db_file = tempfile.mkstemp()
     app = create_app({
         'AUTH_PROVIDERS': ['local'],
@@ -14,14 +13,22 @@ def app():
         'SQLALCHEMY_TRACK_MODIFICATIONS': False
     })
 
-    with app.app_context():
-        yield app
+    yield app
 
     os.close(db_fd)
     os.unlink(db_file)
 
+@pytest.fixture(autouse=True)
+def app(app_primitive):
+    with app_primitive.app_context():
+        db.session.commit = lambda: None
 
-@pytest.fixture(scope="session")
+        yield app_primitive
+
+        db.session.rollback()
+
+@pytest.fixture()
 def client(app):
     with app.test_client() as client:
         yield client
+
