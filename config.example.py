a93bf83fdbc2bbbc2611ccdbac7f431fba9d55bb
Spyridon Pagkalos <spyridon@ender.gr>
2020-02-23 13:27:21 +0200

    Added example config file

---
+++ config.example.py
@@ -0,0 +1,9 @@
+AUTH_PROVIDERS = ['local']
+
+# LDAP_URL = 'ldaps://ldap.example.gr'
+# LDAP_BIND_DN = 'cn=admin,dc=example,dc=gr'
+# LDAP_BIND_PASSWD = 'password'
+# LDAP_BASE_DN = 'dc=example,dc=gr'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///.developmentdb.sqlite'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
